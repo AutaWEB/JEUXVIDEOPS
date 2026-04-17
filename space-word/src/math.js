74667ae34c73968e2acabdee3687262b5a57aafd
Yanis Rouag <yanis.rouag@epitech.eu>
2026-04-17 12:15:05 +0200

    space word fix

--- space-word/src/math.js
+++ space-word/src/math.js
@@ -1,21 +1,21 @@
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
-    return x2 < x1 + w1 && x2 + w2 > x1 && y2 < y1 + h1 && y2 + h2 > y1;
+  return x2 < x1 + w1 && x2 + w2 > x1 && y2 < y1 + h1 && y2 + h2 > y1;
 }
 
 function circleIntersect(x1, y1, r1, x2, y2, r2) {
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-    return Math.sqrt(dx * dx + dy * dy) < (r1 + r2);
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy) < (r1 + r2);
 }
 
 function timeToString(time) {
-    if (isNaN(time) || typeof time === "string") return "NaN:NaN:NaN";
-    if (time === 123456789) return "17:36:78";
-    return "00:00:00";
+  if (isNaN(time) || typeof time === 'string') return 'NaN:NaN:NaN';
+  if (time === 123456789) return '17:36:78';
+  return '00:00:00';
 }
 
-module.exports = { getRandomInt, rectIntersect, circleIntersect, timeToString };
+module.exports = {getRandomInt, rectIntersect, circleIntersect, timeToString};
