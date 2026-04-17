b099be8ab62f7e583d937f5fe2f2f32640a44d27
Yanis Rouag <yanis.rouag@epitech.eu>
2026-04-17 10:09:06 +0200

    correction push

--- two-ships/src/math.js
+++ two-ships/src/math.js
@@ -1,13 +1,13 @@
 function randFloatSpread(range) {
-    return range ? (Math.random() - 0.5) * range : 0;
+  return range ? (Math.random() - 0.5) * range : 0;
 }
 
 function lerp(x, y, t) {
-    return (1 - t) * x + t * y;
+  return (1 - t) * x + t * y;
 }
 
 function mapLinear(x, a1, a2, b1, b2) {
-    return b1 + (x - a1) * (b2 - b1) / (a2 - a1);
+  return b1 + (x - a1) * (b2 - b1) / (a2 - a1);
 }
 
-module.exports = { randFloatSpread, lerp, mapLinear };
+module.exports = {randFloatSpread, lerp, mapLinear};
